c28a758bac5e1ccba9c3e967696a5c2f5500ac5b
Wabognie <tiphaine.casy@hotmail.fr>
2020-12-26 16:21:40 +0100

    obligation argues

--- index.py
+++ index.py
@@ -7,37 +7,18 @@ Tiphaine Casy
 ################IMPORT SECTION################
 import tools_karkkainen_sanders as tks
 import pandas as pd
-import sys
 import argparse
+import time
 
 ################PARSER SECTION################
-"""
-TO DO : try execpt pour obliger a mettre les arguments
-"""
-
-"""
-    Try - except parser : force to enquire the 2 argues to run correctly the code
-    "help" part : if user need some information to run code write "python3 index.py -help" so all informations are write on terminal
-    To use argues and return the user input : "args.nameofargue" (ex : "args.ref" or "args.out" in this case)
-"""
 parser = argparse.ArgumentParser()
-parser.add_argument("--ref", help="Path of genome file (.fasta)")
-parser.add_argument("--out", help="Path of the results file (.dp)")
+parser.add_argument("--ref", help="Path of genome file (.fasta)", type = str, default=argparse.SUPPRESS)
+parser.add_argument("--out", help="Path of the results file (.dp)", type = str, default=argparse.SUPPRESS)
 args = parser.parse_args()
 
-################READ AND KEEPBACK SEQUENCE OF INPUT FILE################
-"""
-    To use the Burrows Wheeler algorithm we need to add a "$" at the end of sequence to be usable by the other functions
-"""
-ref = open(str(args.ref), 'r')
-sequence = ''
-for line in ref :
-    line = str(line).replace('\n','') ##add to delete line break
-    if '>' not in line :
-        sequence = str(line) + '$'
 
-################SA[i] KEEP BACK################
-sa = tks.simple_kark_sort(sequence) ##keep back of SA[i] calculated thanks to tools_karkkainen_sanders
+################TIME COUNT################
+start = time.time()
 
 ################F,i,BWT KEEP BACK################
 def get_BWT(sequence):
@@ -68,13 +49,44 @@ def get_BWT(sequence):
         BWT.append(i[-1])
         F.append(i[0])
     return BWT, F
-results_BWT = get_BWT(sequence)
 
-################CREATION OF OUT FILE################
-"""
-Save the index in a file as dataframe format, path indication in argues at the beginning
-"""
+################CHECK IF PARSER IS FULL################
+if format(args) != 'Namespace()':
+    ref = open(str(args.ref), 'r')
 
-d = {'SA[i]' : sa, 'F' : results_BWT[1], 'BWT' : results_BWT[0]}
-df = pd.DataFrame(data = d)
-df.to_csv(str(args.out), encoding= 'utf-8', index=False, mode = 'w', header = True)
+    ################READ AND KEEPBACK SEQUENCE OF INPUT FILE################
+    """
+    To use the Burrows Wheeler algorithm we need to add a "$" at the end of sequence to be usable by the other functions
+    """
+    sequence = ''
+    for line in ref :
+        line = str(line).replace('\n','') ##add to delete line break
+        if '>' not in line :
+            sequence = str(line) + '$'
+
+
+    ################SA[i] KEEP BACK################
+    sa = tks.simple_kark_sort(sequence) ##keep back of SA[i] calculated thanks to tools_karkkainen_sanders
+    get_BWT(sequence)
+
+    ################CREATION OF OUT FILE################
+    """
+    Save the index in a file as dataframe format, path indication in argues at the beginning
+    """
+
+    d = {'SA[i]' : sa, 'F' : get_BWT(sequence)[-1], 'BWT' : get_BWT(sequence)[0]}
+    df = pd.DataFrame(data = d)
+    df.to_csv(str(args.out), encoding= 'utf-8', index=False, mode = 'w', header = True)
+
+else :
+    print("Obligation to inform all argues to search substitutions \n")
+    print("For some help write : \'mapper.py --help\' in control terminal")
+
+
+
+
+################TIME COUNT################
+end = time.time()
+
+time_to_analyse = end-start
+print("TIME FOR ANALYSE : " + str(round(time_to_analyse,3)) + " secondes")
